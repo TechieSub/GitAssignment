fc6ebf1b9474c4ca08407b0a1115429038ea2fa5
TechieSub <thakursubham75.950@gmail.com>
2023-09-20 19:07:17 +0530

    First commit

---
+++ GitAssignment01.java
@@ -0,0 +1,17 @@
+public class GitAssignment01 {
+    public static void main(String[] args) {
+        System.out.println("1.What is Git? "+"Ans : It is the Distributed Version Control System");
+        System.out.println("2.What do you understand by term ‘Version Control System? " +
+                "Ans : Name says all about it but It's a software that helps developer manage codes changes over time " +
+                "that's why it's known as version bcoz everytime changes made to code gives it a" +
+                "new version of it");
+        System.out.println("3.What is GitHub? " +"Ans : It's remote server that helps manage and store codes");
+        System.out.println("4.mention some popular Git hosting Services " +"Ans : Bitbucket, Github, GitLab");
+        System.out.println("5.Different type of version control Systems "+"Ans :There are 3 - 1. Local VCS, 2." +
+                " Centralized VCS, 3. Distributed VCS");
+        System.out.println("6.What benefits come with using Git? "+"Ans : 1. Distributed VCS, 2.Collaboration," +
+                " 3.Track History, 4.Branching, 5.Data Backup, 6.Code Management");
+        System.out.println("7.What is a Git repository? "+"Ans : Manages history and changes made to codes/files ");
+        System.out.println("8.How can you initialise a repository in Git? "+"Ans : By using this command : Git init");
+    }
+}
